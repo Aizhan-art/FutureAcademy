69cbc4776d5a6233cb6c75c9ac592bd7386a16cc
jenishdatka <jenishdatka@gmail.com>
2025-04-30 17:41:55 +0600

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 urlpatterns = [
     path('api/diary-schedule/', views.DiaryScheduleAPIView.as_view(), name='diary-schedule'),
-
+    
+    #это первая страница
+    path('projects/', ProjectListView.as_view(), name='project_list'),
+    path('events/', EventListView.as_view(), name='event_list'),
+    path('student_activities/', StudentActivityListView.as_view(), name='student_activity_list'),
+    path('children/', ChildrenListView.as_view(), name='children_list'),
+    #новости
+    path('news/', NewsListView.as_view(), name='news_list'),
+    #достижения
+    path('student-activities/', StudentActivityListView.as_view(), name='student_activity_list'),
 ]

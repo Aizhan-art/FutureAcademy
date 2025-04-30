4e4e9574e4b814bdecb7b2e5df6ed442cfb3d419
jenishdatka <jenishdatka@gmail.com>
2025-04-30 19:16:50 +0600

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-
+path('register/', UserRegisterView.as_view(), name='user-register'),
+    path('login/', UserLoginView.as_view(), name='user-login'),
+    path('logout/', UserLogoutView.as_view(), name='user-logout'),
 ]

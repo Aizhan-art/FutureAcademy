6728423fa26342f56f0c498ced082e7357da19ad
jenishdatka <jenishdatka@gmail.com>
2025-04-30 17:32:53 +0600

    Update serializers.py

--- project/serializers.py
+++ project/serializers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
 from .models import LessonSchedule, Diary
+from rest_framework import serializers
+from .models import Project, Task, Event, StudentActivity, News, StudentAchievement
+from django.contrib.auth import get_user_model
+
+MyUser = get_user_model()
 
 class LessonWithGradeSerializer(serializers.ModelSerializer):
     grade = serializers.SerializerMethodField()
@@ -14,5 +19,62 @@ class LessonWithGradeSerializer(serializers.ModelSerializer):
         return diary.grade if diary else None
 
 
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'is_active', 'responsible_user']
 
+
+class ProjectSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = ['id', 'title', 'description', 'main_cover', 'deadline', 'tasks']
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['id', 'title', 'start_date', 'end_date', 'grade']
+
+
+class StudentActivitySerializer(serializers.ModelSerializer):
+    student_name = serializers.CharField(source='student.first_name', read_only=True)
+
+    class Meta:
+        model = StudentActivity
+        fields = ['id', 'student_name', 'description', 'date']
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MyUser
+        fields = ['id', 'first_name', 'last_name', 'avatar', 'grade']
+
+
+
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = ['id', 'title', 'description', 'image', 'created_at']
+
+class StudentAchievementSummarySerializer(serializers.ModelSerializer):
+    last_achievement = serializers.SerializerMethodField()
+    total_participations = serializers.SerializerMethodField()
+    total_victories = serializers.SerializerMethodField()
+
+    class Meta:
+        model = MyUser
+        fields = ['id', 'first_name', 'last_name', 'grade', 'last_achievement', 'total_participations', 'total_victories']
+
+    def get_last_achievement(self, obj):
+        achievement = StudentAchievement.objects.filter(student=obj).order_by('-date').first()
+        return achievement.title if achievement else None
+
+    def get_total_participations(self, obj):
+        return StudentAchievement.objects.filter(student=obj).count()
+
+    def get_total_victories(self, obj):
+        return StudentAchievement.objects.filter(student=obj, is_victory=True).count()
 

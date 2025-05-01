f7484da3009206fdf6e846aa01492e2248517315
Aizhan <osrarova.i@icloud.com>
2025-05-01 20:51:42 +0600

    add chaturls

--- chat/urls.py
+++ chat/urls.py
@@ -3,12 +3,7 @@ from . import views
 
 
 urlpatterns = [
-
     path('conversations/', views.ConversationListCreateAPIView.as_view(), name='conversation-list-create'),
-
-    path('conversations/<int:conversation_id>/messages/', views.MessageListCreateAPIView.as_view(),
-         name='message-list-create'),
-
-    path('conversations/<int:conversation_id>/mark_read/', views.MarkMessagesReadAPIView.as_view(),
-         name='mark-messages-read'),
+    path('conversations/<int:conversation_id>/messages/', views.MessageListCreateAPIView.as_view(),name='message-list-create'),
+    path('conversations/<int:conversation_id>/mark_read/', views.MarkMessagesReadAPIView.as_view(),name='mark-messages-read'),
 ]
